dfd4f40c0746db6719b550824e35268b33ea9cba
Marzena Włoczyk <mk.wloczyk@gmail.com>
2023-12-05 15:03:23 +0100

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,13 +4,9 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-center">
         <div className="hero-title">
-          <h1>Contentful CMS</h1>
+          <h1>React w Realu</h1>
           <p>
-            Single-origin coffee deep v sus small batch. Gorpcore vape
-            lumbersexual normcore four dollar toast drinking vinegar. Twee 90's
-            taiyaki small batch bitters, bespoke jianbing leggings marxism
-            keytar pabst adaptogen mixtape tumblr stumptown. DSA twee iceland
-            tacos swag tote bag. Solarpunk man bun fingerstache retro pinterest.
+            React pozwala na przyjemne tworzenie dynamicznych aplikacji internetowych lub mobilnych. Programowanie w React skupia się na komponentach, które są samodzielnie działającymi, wielokrotnie używanymi blokami kodu. Poniżej przedstawiam projekty stworzone z wykorzystaniem biblioteki React. 
           </p>
         </div>
         <div className="img-container">
